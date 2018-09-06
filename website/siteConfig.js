6152798d1b9f1c6be14ef3fe7f31a64c557a7e39
David Goss <dgoss@whiteclarkegroup.com>
2018-09-06 10:08:50 +0100

    remove docusaurus logos

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -47,8 +47,8 @@ const siteConfig = {
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/docusaurus.svg',
-  footerIcon: 'img/docusaurus.svg',
+  // headerIcon: 'img/docusaurus.svg',
+  // footerIcon: 'img/docusaurus.svg',
   favicon: 'img/favicon.png',
 
   /* Colors for website */
