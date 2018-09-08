0492821f255332787797bd234b6e828b427182fa
David Goss <david@davidgoss.co.uk>
2018-09-08 16:22:09 +0100

    fix a few bits of wrong in the header and footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -22,16 +22,6 @@ class Footer extends React.Component {
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-                height="58"
-              />
-            )}
-          </a>
           <div>
             <h5>Docs</h5>
             <a href={this.docUrl('install', this.props.language)}>
@@ -58,9 +48,9 @@ class Footer extends React.Component {
         </section>
 
         <a
-          href="https://code.facebook.com/projects/"
+          href="https://www.whiteclarkegroup.com/"
           target="_blank"
-          rel="noreferrer noopener"
+          rel="noopener"
           className="fbOpenSource">
           <img
             src={`${this.props.config.baseUrl}img/wcg-logo.svg`}
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -81,7 +81,7 @@ const siteConfig = {
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: ['//buttons.github.io/buttons.js'],
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
@@ -89,8 +89,8 @@ const siteConfig = {
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
+  // ogImage: 'img/docusaurus.png',
+  // twitterImage: 'img/docusaurus.png',
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
