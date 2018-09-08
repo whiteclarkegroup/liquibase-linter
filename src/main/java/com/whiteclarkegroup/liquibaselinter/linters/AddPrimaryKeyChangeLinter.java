5cb72d9b3f4cabc7eede8051390cd2f850bf657e
David Goss <david@davidgoss.co.uk>
2018-09-08 22:11:49 +0100

    remove superfluous rule, rename remaining one

--- src/main/java/com/whiteclarkegroup/liquibaselinter/linters/AddPrimaryKeyChangeLinter.java
+++ src/main/java/com/whiteclarkegroup/liquibaselinter/linters/AddPrimaryKeyChangeLinter.java
@@ -14,8 +14,7 @@ public class AddPrimaryKeyChangeLinter implements Linter<AddPrimaryKeyChange> {
     public void lint(AddPrimaryKeyChange change, RuleRunner ruleRunner) throws ChangeLogParseException {
         getObjectNameLinter().lintObjectNameLength(change.getConstraintName(), change, ruleRunner);
         ruleRunner.forChange(change)
-                .run(RuleType.PRIMARY_KEY_MUST_BE_NAMED, change.getConstraintName())
-                .run(RuleType.PRIMARY_KEY_MUST_USE_TABLE_NAME, change.getConstraintName());
+                .run(RuleType.PRIMARY_KEY_NAME, change.getConstraintName());
     }
 
     ObjectNameLinter getObjectNameLinter() {
--- src/test/java/com/whiteclarkegroup/liquibaselinter/linters/AddPrimaryKeyChangeLinterTest.java
+++ src/test/java/com/whiteclarkegroup/liquibaselinter/linters/AddPrimaryKeyChangeLinterTest.java
@@ -40,18 +40,6 @@ class AddPrimaryKeyChangeLinterTest {
         verify(objectNameLinter, times(1)).lintObjectNameLength("TEST_PK", addPrimaryKeyChange, ruleRunner);
     }
 
-    @DisplayName("Should validate name in incorrect format")
-    @Test
-    void shouldValidateNameInIncorrectFormat(ChangeSet changeSet, RuleRunner ruleRunner) {
-        AddPrimaryKeyChange addPrimaryKeyChange = new AddPrimaryKeyChange();
-        addPrimaryKeyChange.setChangeSet(changeSet);
-        addPrimaryKeyChange.setTableName("TEST");
-        addPrimaryKeyChange.setConstraintName("TEST_PK_ABC");
-        ChangeLogParseException changeLogParseException =
-                assertThrows(ChangeLogParseException.class, () -> addPrimaryKeyChangeLinter.lint(addPrimaryKeyChange, ruleRunner));
-        assertTrue(changeLogParseException.getMessage().contains("Primary key constraint '" + addPrimaryKeyChange.getConstraintName() + "' must end with '_PK' e.g. TABLE_PK"));
-    }
-
     @DisplayName("Should validate name in correct format")
     @Test
     void shouldValidateNameInCorrectFormat(ChangeSet changeSet, RuleRunner ruleRunner) {
@@ -81,20 +69,6 @@ class AddPrimaryKeyChangeLinterTest {
         } catch (ChangeLogParseException e) {
             fail(e);
         }
-    }
-
-    @DisplayName("Should not validate if name in format but validate ending when longer than max length")
-    @Test
-    void shouldNotValidateNameInFormatButValidateEndingWhenMoreThanMaxLength(ChangeSet changeSet, RuleRunner ruleRunner) {
-        AddPrimaryKeyChange addPrimaryKeyChange = new AddPrimaryKeyChange();
-        addPrimaryKeyChange.setChangeSet(changeSet);
-        addPrimaryKeyChange.setTableName("TEST_TEST_TEST_TEST_TEST_TEST");
-        addPrimaryKeyChange.setConstraintName("INVALID_NAME");
-        changeSet.addChange(addPrimaryKeyChange);
-        ChangeLogParseException changeLogParseException =
-                assertThrows(ChangeLogParseException.class, () -> addPrimaryKeyChangeLinter.lint(addPrimaryKeyChange, ruleRunner));
-        assertTrue(changeLogParseException.getMessage().contains("Primary key constraint '" + addPrimaryKeyChange.getConstraintName() + "' must " +
-                "end with '_PK' e.g. TABLE_PK"));
     }
 
 }
