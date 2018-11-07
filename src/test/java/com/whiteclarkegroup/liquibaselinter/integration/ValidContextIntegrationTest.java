498a2d91f4988e7fcf9bf6b26761f2bf3b8a070f
Luke Rogers <lrogers@whiteclarkegroup.com>
2018-11-07 10:59:17 +0000

    Valid context integration test

---
+++ src/test/java/com/whiteclarkegroup/liquibaselinter/integration/ValidContextIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.whiteclarkegroup.liquibaselinter.integration;
+
+import com.whiteclarkegroup.liquibaselinter.resolvers.LiquibaseIntegrationTestResolver;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+@ExtendWith(LiquibaseIntegrationTestResolver.class)
+class ValidContextIntegrationTest extends LinterIntegrationTest {
+
+    @Override
+    List<IntegrationTestConfig> getTests() {
+        IntegrationTestConfig test1 = IntegrationTestConfig.shouldFail(
+            "Should fail with invalid context value",
+            "valid-context/valid-context-fail.xml",
+            "valid-context/lqllint.json",
+            "Context is incorrect, should end with '_test' or '_script'");
+
+        IntegrationTestConfig test2 = IntegrationTestConfig.shouldPass(
+            "Should pass with valid context value",
+            "valid-context/valid-context-pass.xml",
+            "valid-context/lqllint.json");
+
+        return Arrays.asList(test1, test2);
+    }
+
+}
