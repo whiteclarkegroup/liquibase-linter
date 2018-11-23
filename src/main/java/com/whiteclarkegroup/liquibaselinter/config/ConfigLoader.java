9a2c6b383a07ebd2e2c9c1be61be408bd854a4b9
Luke Rogers <8382642+luke-rogers@users.noreply.github.com>
2018-11-23 11:03:59 +0000

    Config loader path (#44)
    * Config loader should support both lqllint.json and lqlint.json
    
    * Config loader should support both lqllint.json and lqlint.json
    
    * Test config loading from different file names

--- src/main/java/com/whiteclarkegroup/liquibaselinter/config/ConfigLoader.java
+++ src/main/java/com/whiteclarkegroup/liquibaselinter/config/ConfigLoader.java
@@ -9,20 +9,31 @@ import java.util.Set;
 
 public class ConfigLoader {
 
+    public static final String LQLINT_CONFIG = "/lqlint.json";
     public static final String LQLLINT_CONFIG = "/lqllint.json";
 
     public Config load(ResourceAccessor resourceAccessor) {
         try {
-            Set<InputStream> resourcesAsStream = resourceAccessor.getResourcesAsStream(LQLLINT_CONFIG);
-            if (resourcesAsStream != null && !resourcesAsStream.isEmpty()) {
-                try (InputStream inputStream = resourcesAsStream.iterator().next()) {
-                    return Config.fromInputStream(inputStream);
-                }
+            Config config;
+            if ((config = loadConfig(resourceAccessor, LQLINT_CONFIG)) != null) {
+                return config;
+            } else if ((config = loadConfig(resourceAccessor, LQLLINT_CONFIG)) != null) {
+                return config;
             }
         } catch (IOException e) {
             throw new UnexpectedLiquibaseException("Failed to load lq lint config file", e);
         }
         throw new UnexpectedLiquibaseException("Failed to load lq lint config file");
+    }
+
+    private Config loadConfig(ResourceAccessor resourceAccessor, String path) throws IOException {
+        final Set<InputStream> resourcesAsStream = resourceAccessor.getResourcesAsStream(path);
+        if (resourcesAsStream != null && !resourcesAsStream.isEmpty()) {
+            try (InputStream inputStream = resourcesAsStream.iterator().next()) {
+                return Config.fromInputStream(inputStream);
+            }
+        }
+        return null;
     }
 
 }
