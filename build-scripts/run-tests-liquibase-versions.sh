5cf336e084296272e339e567fa030ca94d860faa
Luke Rogers <lrogers@whiteclarkegroup.com>
2019-07-31 13:30:47 +0100

    Tidy up

--- build-scripts/run-tests-liquibase-versions.sh
+++ build-scripts/run-tests-liquibase-versions.sh
@@ -2,7 +2,6 @@
 set -ev
 declare -a VERSIONS=("[3.5.0,3.6.0)" "[3.6.0,3.7.0)")
 for version in "${VERSIONS[@]}"; do
-  rm -f databasechangelog.csv
-  rm -f databasechangelog.csv.new
+  rm -f databasechangelog.csv databasechangelog.csv.new
   mvn clean test -B -Dliquibase.version=$version
 done
