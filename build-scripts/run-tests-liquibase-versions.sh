0b49c467dc810c916380c9a75f695be56d26a9a2
Luke Rogers <lrogers@whiteclarkegroup.com>
2019-07-31 12:29:54 +0100

    Add support for latest liquibase version 3.7.0

--- build-scripts/run-tests-liquibase-versions.sh
+++ build-scripts/run-tests-liquibase-versions.sh
@@ -2,7 +2,7 @@
 set -ev
 declare -a VERSIONS=("[3.5.0,3.6.0)" "[3.6.0,3.7.0)")
 for version in "${VERSIONS[@]}"; do
-  rm -f ./databasechangelog.csv
-  rm -f ./databasechangelog.csv.new
+  rm -f databasechangelog.csv
+  rm -f databasechangelog.csv.new
   mvn clean test -B -Dliquibase.version=$version
 done
