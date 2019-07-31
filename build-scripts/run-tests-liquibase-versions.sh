bbc668bb4bc3908f5b0c89f4ea97399fb4a1fb76
Luke Rogers <lrogers@whiteclarkegroup.com>
2019-07-31 18:08:10 +0100

    Add test coverage for 3.4.x with yaml requirement workaround

--- build-scripts/run-tests-liquibase-versions.sh
+++ build-scripts/run-tests-liquibase-versions.sh
@@ -5,3 +5,6 @@ for version in "${VERSIONS[@]}"; do
   rm -f databasechangelog.csv databasechangelog.csv.new
   mvn clean test -B -Dliquibase.version=$version
 done
+# yaml was an optional dependency until 3.5+ so have to work around with profile
+rm -f databasechangelog.csv databasechangelog.csv.new
+mvn test -B -Dliquibase.version=3.4.2 -Dyaml.version=1.13
