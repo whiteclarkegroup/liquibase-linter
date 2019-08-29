cc8db19f4e92ada425f14e6a96d66cb01198c975
Luke Rogers <8382642+luke-rogers@users.noreply.github.com>
2019-08-29 07:57:33 +0100

    Add 3.8.0 liquibase version (#77)

--- build-scripts/run-tests-liquibase-versions.sh
+++ build-scripts/run-tests-liquibase-versions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-declare -a VERSIONS=("[3.5.0,3.6.0)" "[3.6.0,3.7.0)")
+declare -a VERSIONS=("[3.5.0,3.6.0)" "[3.6.0,3.7.0)" "[3.7.0,3.8.0)")
 for version in "${VERSIONS[@]}"; do
   rm -f databasechangelog.csv databasechangelog.csv.new
   mvn clean test -B -Dliquibase.version=$version
