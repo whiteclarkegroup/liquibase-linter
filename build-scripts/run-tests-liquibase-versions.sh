6c53167ea702bae4d1e7f3fc12aeb1a41e8baa78
Luke Rogers <8382642+luke-rogers@users.noreply.github.com>
2019-11-12 10:40:46 +0000

    Better snakeyml dependency management (#84)

--- build-scripts/run-tests-liquibase-versions.sh
+++ build-scripts/run-tests-liquibase-versions.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -ev
-declare -a VERSIONS=("[3.5.0,3.6.0)" "[3.6.0,3.7.0)" "[3.7.0,3.8.0)")
-for version in "${VERSIONS[@]}"; do
-  rm -f databasechangelog.csv databasechangelog.csv.new
-  mvn clean test -B -Dliquibase.version=$version
+declare -a VERSIONS=("[3.4.0,3.5.0)" "[3.5.0,3.6.0)" "[3.6.0,3.7.0)" "[3.7.0,3.8.0)")
+declare -a YAMLVERSION=("1.13" "1.17" "1.18" "1.23")
+for (( i = 0; i < ${#VERSIONS[@]}; ++i )); do
+    rm -f databasechangelog.csv databasechangelog.csv.new
+    mvn clean test -B -Dliquibase.version=${VERSIONS[i]} -Dyaml.version=${YAMLVERSION[i]}
 done
-# yaml was an optional dependency until 3.5+ so have to work around with profile
-rm -f databasechangelog.csv databasechangelog.csv.new
-mvn test -B -Dliquibase.version=3.4.2 -Dyaml.version=1.13
